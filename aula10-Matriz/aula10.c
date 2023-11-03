aef9d5f5f1647f9a8336bfb5728c65fa4208e6ee
thalesacardoso <thales.dev@hotmail.com>
2023-11-02 22:01:15 -0300

    aula 10

---
+++ aula10-Matriz/aula10.c
@@ -0,0 +1,40 @@
+
+// O comando include serve para incluir bibliotecas de funções
+// <stdio.h>  Funções de entrada e saída de dados
+// <string.h>  Funções de string
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int M, N, i, j;
+
+    printf("Quantas linhas vai ter a matriz? ");
+    scanf("%d", &M);
+    printf("Quantas colunas vai ter a matriz? ");
+    scanf("%d", &N);
+
+    int mat[M][N]; // Declaração de uma matriz
+
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < N; j++) {
+            printf("Elemento [%d,%d]: ", i, j);
+            scanf("%d", &mat[i][j]);
+        }
+    }
+
+    printf("\nMATRIZ DIGITADA:\n");
+
+    for (i = 0; i < M; i++) {
+        for (j = 0; j < N; j++) {
+            printf("%d ", mat[i][j]);
+        }
+
+        printf("\n");
+    }
+
+
+    return 0;
+}
+
