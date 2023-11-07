965f9e753fd25853173e8d150c2cee01633a8b2d
thalesacardoso <thales.dev@hotmail.com>
2023-11-07 10:39:09 -0300

    Exercício 02 - Medidas do retangulo

---
+++ exercicio02-retangulo/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Problema "retangulo"
+Fazer um programa para ler as medidas da base e altura de um retângulo.
+Em seguida, mostrar o valor da área, perímetro e diagonal deste retângulo,
+com quatro casas decimais, conforme exemplos.
+*/
+
+int main()
+{
+    double base, altura, area , perimetro, diagonal;
+
+    printf("Base do retangulo: ");
+    scanf("%lf", &base);
+
+    printf("Altura do retangulo: ");
+    scanf("%lf", &altura);
+
+    area = base * altura;
+    perimetro = 2 * (base + altura);
+    diagonal = sqrt(pow(base, 2.0 ) + pow(altura, 2.0 ));
+
+    printf("Area: %.4lf \n", area);
+    printf("Perimetro %.4lf \n", perimetro);
+    printf("Diagonal %.4lf", diagonal);
+
+    return 0;
+}
