ee59ac40ed958be3f798ee7810288c5dcd705f2b
thalesacardoso <thales.dev@hotmail.com>
2023-11-07 12:13:24 -0300

    Exercício 05 - Fórmula de baskara

---
+++ exercicio05-baskara/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Problema "baskara"
+Fazer um programa para ler os três coeficientes de uma equação do segundo grau. Usando a fórmula
+de Baskara, calcular e mostrar os valores das raízes x1 e x2 da equação com quatro casas decimais,
+conforme exemplo. Se a equação não possuir raízes reais, mostrar uma mensagem.
+*/
+int main()
+{
+    double a, b, c, x1, x2, delta;
+
+    printf("Coeficiente a: ");
+    scanf("%lf", &a);
+
+    printf("Coeficiente b: ");
+    scanf("%lf", &b);
+
+    printf("Coeficiente c: ");
+    scanf("%lf", &c);
+
+    delta = (b * b) -4 * a * c;
+
+    if(delta == 0 || delta < 0) {
+        printf("Esta equacao nao possui raizes reais");
+    } else {
+        x1 = (-b + sqrt(delta)) / (2 * a);
+        x2 = (-b - sqrt(delta)) / (2 * a);
+        printf("X1 = %.4lf \n", x1);
+        printf("X2 = %.4lf", x2);
+    }
+
+    return 0;
+}
